24c82de05b7abb422bb7f4f52187fdcd4fba758c
Matheus Calonico <matheusscalonico@gmail.com>
2026-06-26 19:10:14 -0300

    Fix Vercel bundled db.json

--- api/server.js
+++ api/server.js
@@ -7,8 +7,9 @@ const server = jsonServer.create();
 // Copia o db.json para a pasta temporária /tmp que permite escrita
 const dbPath = path.join('/tmp', 'db.json');
 if (!fs.existsSync(dbPath)) {
-  const originalDb = fs.readFileSync(path.join(process.cwd(), 'db.json'), 'utf8');
-  fs.writeFileSync(dbPath, originalDb);
+  // Usamos require para forçar o empacotador do Vercel a incluir o arquivo no deploy
+  const originalDb = require('../db.json');
+  fs.writeFileSync(dbPath, JSON.stringify(originalDb));
 }
 
 const router = jsonServer.router(dbPath);

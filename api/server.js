e3d0a8a8b289176c5a7e95297f3453d2aeaf400d
Matheus Calonico <matheusscalonico@gmail.com>
2026-06-26 19:23:16 -0300

    Fix API in memory db

--- api/server.js
+++ api/server.js
@@ -4,15 +4,9 @@ const path = require('path');
 
 const server = jsonServer.create();
 
-// Copia o db.json para a pasta temporária /tmp que permite escrita
-const dbPath = path.join('/tmp', 'db.json');
-if (!fs.existsSync(dbPath)) {
-  // Usamos require para forçar o empacotador do Vercel a incluir o arquivo no deploy
-  const originalDb = require('../db.json');
-  fs.writeFileSync(dbPath, JSON.stringify(originalDb));
-}
-
-const router = jsonServer.router(dbPath);
+// Carrega o banco de dados original (em memória)
+const db = require('../db.json');
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
